984e1067b032af1989dd2419b4720b4b2bc5843c
CoertSchrijver <coert.schrijver@gmail.com>
2017-11-17 15:14:36 +0100

    Subregions and Hubs
    Visual representation of the subregions with hubs can be seen as an x,y-diagram where
      x and y are defined for [0,300], equally split in 9 regions; with hubs at the center of each region.
      I felt like this was the simplest method when it comes to calculating distances between regions once we have to.
    
      The regions have, so far, only been implemented in the Seller class. I will refrain from implementing it
      in the remainder of the model until the shipments are fully functional. The hubs also have no functionality at the moment,
      just a plain location in the x,y-diagram.

--- config/config.py
+++ config/config.py
@@ -1,6 +1,23 @@
+from random import randint
 class Config(object):
     price_range = [0, 10]  # say for a uniform distribution
     quantity_range = [100, 1500]  # again uniform?
-    region = [[0, 200], [0, 200]]
     run_length = 250
     random_agent_creation = .05
+    number_of_subregions = 9
+    subregionx = 100
+    subregiony = 100
+    region = [] #Where there are 9 subregions, numbering from 0 to 8
+    hubs = []
+    for i in range(int(number_of_subregions/3)):
+        for j in range(int(number_of_subregions/3)):
+            region.append([[j*subregionx, (j+1)*subregionx],[(i)*subregiony, (i+1)*subregiony]])
+            hubs.append([(j+1)*subregionx - .5*subregionx, (i+1)*subregiony - .5*subregiony])
+
+ 
+
+    print(randint(*region[4][0])) #Test-run for x-coordinates in subregion i=4, such that region[i][0]
+    print(randint(*region[4][1])) #Test-run for y-coordinates in subregion i=4, such that region[i][1]
+    print([randint(*region[4][0]), randint(*region[4][1])]) #Test-run for location in subregion i = 4
+
+
--- entities/seller.py
+++ entities/seller.py
@@ -7,14 +7,15 @@ from typing import NamedTuple
 
 class Seller(Agent):
 
+
     def __init__(self):
         self.agent_id = 0
         self.action = 0
         self.quantity = randint(*Config.quantity_range)
         self.item_price = randint(*Config.price_range)
-
-        self.location = [randint(*Config.region[0]),  # x axis
-                         randint(*Config.region[1])]  # y axis
+        region_generate = randint(0, 8)
+        self.location = [randint(*Config.region[region_generate][0]),  # x axis
+                         randint(*Config.region[region_generate][1])]  # y axis
 
     def bid(self) -> NamedTuple:
         return self.Bid(action=BiddingTypes.SELL,
