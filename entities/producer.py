78b2c0d2213ea8748612866c0c692e30e343b87a
CoertSchrijver <coert.schrijver@gmail.com>
2017-12-09 17:02:33 +0100

    Update shipments 9/12

--- entities/producer.py
+++ entities/producer.py
@@ -5,19 +5,14 @@ from random import randint
 class Producer(Seller):
     def __init__(self):
         super(Producer, self).__init__()
-
     def produce(self):
-        for _ in range(*Config.production_rate):
+      shipments = []
+      shipments_IDs = {}
+      for _ in range(randint(*Config.production_rate)):
             destination_shipment= [randint(*Config.region[0]),  # x axis
-                            randint(*Config.region[1])]  # y axis
-            shipments.append([self.location, destination_shipment, id(seller)])
-
-
-
-as
-"""Ik heb twijfels over in hoeverre dit de gewenste vorm voor shipment is. Bovendien vraag ik me af of
-'self.location' goed meegenomen word en of dit hele gebeuren goed uitpakt in de run. Voordat ik het allemaal
-ga proberen te verwerken in run wacht ik even tot het doet wat het moet doen."""
+                                   randint(*Config.region[1])]  # y axis
+            shipments_IDs['Shipment %s' % len(shipments_IDs)] = len(shipments_IDs)
+            shipments.append([self.location, destination_shipment, len(shipments_IDs)]) #TODO self.location should not be one fixed number
 
 
 
